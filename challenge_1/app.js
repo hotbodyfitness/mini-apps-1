308fcfa1adb236c59ac633a36238057b31a81475
Zack Miller <hotbodyfitness@gmail.com>
2019-07-10 09:27:10 -0700

    Added input for player names

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,35 +1,33 @@
-// var table = document.getElementsByClassName('table');
-
-// produces array of 9 elements
-
-
-var xTurn = true;
-var clicked = 0;
-var board = [['__', '__', '__'],
-             ['__', '__', '__'],
-             ['__', '__', '__']];
-var xWins = false;
-var oWins = false;
+var gameState = {
+  xTurn: true,
+  clicked: 0,
+  board: [['__', '__', '__'],
+          ['__', '__', '__'],
+          ['__', '__', '__']],
+  xWins: false,
+  oWins: false,
+  winnerLastRound: 'X'
+};
 
 var boardChecker = () => { // runs after all boxes have been clicked
   for (let z = 0; z < 2; z++) {
     var xOrO = 'X';
     if (z === 1) {xOrO = 'O'}
-    if (xOrO === board[1][1]) {
+    if (xOrO === gameState.board[1][1]) {
       if (checkDiagonalRight(xOrO) || checkDiagonalLeft(xOrO)) {
-        if (xOrO === 'X') {xWins = true;}
-        if (xOrO === 'O') {oWins = true;}
+        if (xOrO === 'X') {gameState.xWins = true;}
+        if (xOrO === 'O') {gameState.oWins = true;}
         break;
       }
     }
-    for (let i in board) {
+    for (let i in gameState.board) {
       if (checkHorizontal(i, xOrO)) {
-        if (xOrO === 'X') {xWins = true;}
-        if (xOrO === 'O') {oWins = true;}
+        if (xOrO === 'X') {gameState.xWins = true;}
+        if (xOrO === 'O') {gameState.oWins = true;}
         break;
       } else if (checkVertical(i, xOrO)) {
-        if (xOrO === 'X') {xWins = true;}
-        if (xOrO === 'O') {oWins = true;}
+        if (xOrO === 'X') {gameState.xWins = true;}
+        if (xOrO === 'O') {gameState.oWins = true;}
         break;
       }
     }
@@ -39,21 +37,30 @@ var boardChecker = () => { // runs after all boxes have been clicked
 var displayWinner = () => {
   var element = document.getElementById('winner');
   var winningText = '';
-  if (xWins === true && oWins === true) {
+  if (gameState.xWins === true && gameState.oWins === true) {
     winningText = 'Cool! You both won this time!';
-  } else if (xWins === true) {
-    winningText = 'Xzibit the skillz! X\'s win!';
-  } else if (oWins === true) {
+  } else if (gameState.xWins === true) {
+    winningText = 'Xzibiting skillz! X\'s win!';
+    gameState.winnerLastRound = 'X';
+  } else if (gameState.oWins === true) {
     winningText = 'Ooh yeah! O\'s win!';
+    gameState.winnerLastRound = 'O';
   } else {
-    winningText = 'You guys both lose. Ouch!';
+    var xOrO = 'X';
+    if (gameState.winnerLastRound === 'X') {
+      gameState.winnerLastRound = 'O';
+      xOrO = 'O';
+    } else {
+      gameState.winnerLastRound = 'X';
+    }
+    winningText = `You guys both lose. Ouch!
+                  ${xOrO}\'s get a turn first this round.`;
   }
   return element.innerText = winningText;
 };
 
-
 var checkHorizontal = (row, xOrO) => {
-  for (let element of board[row]) {
+  for (let element of gameState.board[row]) {
     if (element !== xOrO) {
       return false;
     }
@@ -63,7 +70,7 @@ var checkHorizontal = (row, xOrO) => {
 
 var checkVertical = (col, xOrO) => {
   for (let i = 0; i < 3; i++) {
-    if (board[i][col] !== xOrO) {
+    if (gameState.board[i][col] !== xOrO) {
       return false;
     }
   }
@@ -73,7 +80,7 @@ var checkVertical = (col, xOrO) => {
 var checkDiagonalRight = (xOrO) => {
   var win = true;
   for (let i = 0; i < 3; i+=2) {
-    if (board[i][i] !== xOrO) {
+    if (gameState.board[i][i] !== xOrO) {
       win = false;
     }
   }
@@ -82,22 +89,26 @@ var checkDiagonalRight = (xOrO) => {
 
 var checkDiagonalLeft = (xOrO) => {
   var win = true;
-  if (board[0][2] !== xOrO || board[2][0] !== xOrO) {
+  if (gameState.board[0][2] !== xOrO || gameState.board[2][0] !== xOrO) {
     win = false;
   }
   return win;
 };
 
 var resetGame = () => {
-  clicked = 0;
-  xWins = false;
-  oWins = false;
+  gameState.clicked = 0;
+  gameState.xWins = false;
+  gameState.oWins = false;
   var element = document.getElementById('winner').innerText = '';
   var tableButtons = document.getElementsByClassName('table');
   for (let button of tableButtons) {
     button.innerText = '__';
   }
-  xTurn = true;
+  if (gameState.winnerLastRound === 'X') {
+    gameState.xTurn = true;
+  } else {
+    gameState.xTurn = false;
+  }
 };
 
 var clickHandler = (cellNumber) => {
@@ -106,39 +117,38 @@ var clickHandler = (cellNumber) => {
   if (element.innerText !== '__') {
     alert('This box has already been checked, please try again');
   } else {
-    if (!xTurn) {
+    if (!gameState.xTurn) {
       xOrO = 'O';
-      xTurn = true;
+      gameState.xTurn = true;
     } else {
-      xTurn = false;
+      gameState.xTurn = false;
     }
     element.innerText = xOrO;
 
     // these if-else's add the piece to the board
     if (cellNumber[0] === '0') {
-      for (let x in board[0]) {
+      for (let x in gameState.board[0]) {
         if (Number(x) === Number(cellNumber[1])) {
-          board[0][x] = xOrO;
+          gameState.board[0][x] = xOrO;
         }
       }
     } else if (cellNumber[0] === '1') {
-      for (let x in board[1]) {
+      for (let x in gameState.board[1]) {
         if (Number(x) === Number(cellNumber[1])) {
-          board[1][x] = xOrO;
+          gameState.board[1][x] = xOrO;
         }
       }
     } else { // (cellNumber[0] === '2')
-      for (let x in board[2]) {
+      for (let x in gameState.board[2]) {
         if (Number(x) === Number(cellNumber[1])) {
-          board[2][x] = xOrO;
+          gameState.board[2][x] = xOrO;
         }
       }
     }
-    clicked++;
-    if (clicked === 9) {
+    gameState.clicked++;
+    if (gameState.clicked === 9) {
       boardChecker();
     }
   }
+  console.log('gameState', gameState);
 };
-
-
