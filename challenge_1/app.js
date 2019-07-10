cdfefb5b7bd6e52db526a5d5f2d07bd17c901660
Zack Miller <hotbodyfitness@gmail.com>
2019-07-10 11:23:39 -0700

    Added scoreboard

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -39,12 +39,15 @@ var displayWinner = () => {
   var winningText = '';
   if (gameState.xWins === true && gameState.oWins === true) {
     winningText = 'Cool! You both won this time!';
+    logWinners('XO');
   } else if (gameState.xWins === true) {
     winningText = 'Xzibiting skillz! X\'s win!';
     gameState.winnerLastRound = 'X';
+    logWinners('X');
   } else if (gameState.oWins === true) {
     winningText = 'Ooh yeah! O\'s win!';
     gameState.winnerLastRound = 'O';
+    logWinners('O');
   } else {
     var xOrO = 'X';
     if (gameState.winnerLastRound === 'X') {
@@ -57,6 +60,26 @@ var displayWinner = () => {
                   ${xOrO}\'s get a turn first this round.`;
   }
   return element.innerText = winningText;
+};
+
+var logWinners = (xOrO) => {
+  var xscore = document.getElementById('xscore');
+  var oscore = document.getElementById('oscore');
+  var xDoubleDigitWins = false;
+  var oDoubleDigitWins = false;
+  if (xscore.innerText.length > 4) {xDoubleDigitWins = true;}
+  if (oscore.innerText.length > 4) {oDoubleDigitWins = true;}
+
+  if (xDoubleDigitWins && xOrO.includes('X')) {
+    xscore.innerText = 'X: ' + (Number(xscore.innerText[3] + xscore.innerText[4]) + 1);
+  } else if (xOrO.includes('X')) {
+    xscore.innerText = 'X: ' + (Number(xscore.innerText[3]) + 1);
+  }
+  if (oDoubleDigitWins && xOrO.includes('O')) {
+    oscore.innerText = 'O: ' + (Number(oscore.innerText[3] + oscore.innerText[4]) + 1);
+  } else if (xOrO.includes('O')) {
+    oscore.innerText = 'O: ' + (Number(oscore.innerText[3]) + 1);
+  }
 };
 
 var checkHorizontal = (row, xOrO) => {
@@ -111,6 +134,11 @@ var resetGame = () => {
   }
 };
 
+var resetScore = () => {
+  document.getElementById('xscore').innerText = 'X: 0';
+  document.getElementById('oscore').innerText = 'O: 0';
+};
+
 var clickHandler = (cellNumber) => {
   var element = document.getElementById(cellNumber);
   var xOrO = 'X';
@@ -151,4 +179,20 @@ var clickHandler = (cellNumber) => {
     }
   }
   console.log('gameState', gameState);
+};
+
+var enterName = (xOrO) => {
+  var clicked = document.getElementById(`${xOrO}submit`);
+  var nameDiv = document.getElementById(`${xOrO}name`);
+  var nameDiv2 = document.getElementById(`${xOrO}name2`);
+  if (clicked.value === 'Play') {
+    var name = nameDiv.firstChild.value;
+    nameDiv2.innerHTML = `<b style="float:right; padding-right:0.5em">${name}</b>`;
+    nameDiv.firstChild.value = '';
+    clicked.value = 'Reset';
+  } else {
+    // nameDiv.innerHTML = `<input type="text">`;
+    nameDiv2.innerText = '';
+    clicked.value = 'Play';
+  }
 };
