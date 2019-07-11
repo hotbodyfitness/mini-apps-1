c5e213e6935b5175ebb240cff4b4bd9137f9c1ed
Zack Miller <hotbodyfitness@gmail.com>
2019-07-11 12:45:54 -0700

    Figuring out JSON

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,16 @@
+// var express = require('express');
+// var server = require('../server');
+// var $ = require('jquery');
+
+$(document).ready(function() {
+  // $('#form').text('hey');
+
+  // $('#postButton').on('click', () => {
+  //   var input = $('#jsonInput').val();
+  //   console.log(input);
+  // });
 
 
+
+
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// var $ = require('jquery');
 
+app.listen(8080, () => {
+  console.log('Express is listening on port 8080');
+});
 
+app.use(express.static('client'));
+// app.use(bodyParser.json(/*{ type: 'application/*+json' }*/));
+// app.use(bodyParser.text({ type: 'text/html' }));
+app.use(express.urlencoded({extended:true}));
+
+app.post('/post', (req, res) => {
+  // console.log('REQUEST************', req);
+  console.log('JSON.stringify REQ.body***********', req.body);
+  res.end();
+  // res.render('index');
+});
+
+// module.exports = app;
