61ffe84cf0e1d79704f43ac89b59542e0b3518c4
Zack Miller <hotbodyfitness@gmail.com>
2019-07-15 17:45:58 -0700

    Some progress building React app.jsx, button not working yet

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,42 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  F1() {
+    return (
+      <div id="f1page">
+        <form>
+          <legend>Page F1</legend>
+          <fieldset>
+            <p>Name: </p><input type="text" defaultValue="Your Name"></input>
+            <p>Email: </p><input type="text" defaultValue="email@url.com"></input>
+            <p>Password: </p><input type="text" defaultValue="un1quePa55word"></input>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
 
+  onCheckout(event) {
+    event.preventDefault();
+    var page = document.getElementById(f0page);
+    $(page).html(this.F1);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1><u>Multistep Checkout Experience</u></h1>
+        <form id="f0page">
+        <input type="submit" value="Checkout" onClick={this.onCheckout.bind(this)}></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
